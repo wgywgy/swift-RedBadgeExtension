7f5398a55874befcbfcc7f3e9eaedd5d8bbf69c8
吴冠宇 <wuguanyu@letv.com>
2016-09-13 11:44:21 +0800

    change Red dot to vc property

--- RedBadgeExtension/ViewController.swift
+++ RedBadgeExtension/ViewController.swift
@@ -10,20 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    let aView: UIView = {
         let aView = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         aView.backgroundColor = UIColor.yellowColor()
         aView.badgeColor = UIColor.greenColor()
         aView.showBadge()
+        return aView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
         self.view.addSubview(aView)
-        
         
         let delayInSeconds = 3.0
         let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Double(NSEC_PER_SEC)))
-        dispatch_after(delayTime, dispatch_get_main_queue()) {
-            aView.hideBadge()
+        dispatch_after(delayTime, dispatch_get_main_queue()) { [weak self] in
+            guard let `self` = self else { return }
+            self.aView.hideBadge()
         }
     }
 }
